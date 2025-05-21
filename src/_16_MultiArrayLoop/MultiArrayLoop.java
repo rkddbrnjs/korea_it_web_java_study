e73606a36f27e148ae2be4d35b6214ae62367ae8
rkddbrnjs <rkddbrnjs@naver.com>
2025-05-21 19:15:52 +0900

    Feat: MultiArrayLoop

--- src/_16_MultiArrayLoop/MultiArrayLoop.java
+++ src/_16_MultiArrayLoop/MultiArrayLoop.java
@@ -32,6 +32,29 @@ public class MultiArrayLoop {
             }
             System.out.println();
         }
+        int[][] arr = new int[3][3];
+        int num = 1;
+        //홀수만 다 대입
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = num;
+                num += 2;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+        System.out.println("합계는" + sum);
     }
 }
 

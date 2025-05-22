903cc94175d1a3d0e0f61722c2991d2576867b4f
rkddbrnjs <rkddbrnjs@naver.com>
2025-05-22 19:25:41 +0900

    Feat: 지역변수

---
+++ src/_19_VeriableScope/VeriableScope.java
@@ -0,0 +1,14 @@
+package _19_VeriableScope;
+
+public class VeriableScope {
+    public static void methodA(){
+        String str = "abc";
+    }
+    public static void main(String[] args) {
+        String name = "강유권";
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+    }
+}

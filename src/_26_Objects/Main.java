154833ea4aaef11607e32bfb107b42c37b68f998
rkddbrnjs <rkddbrnjs@naver.com>
2025-05-28 21:42:38 +0900

    Feat: Objects

--- src/_26_Objects/Main.java
+++ src/_26_Objects/Main.java
@@ -2,6 +2,14 @@ package _26_Objects;
 
 public class Main {
     public static void main(String[] args) {
+        //모든 클래스는 Object를 상속받고 있다.
+        //equals가 true가 되려면 해시코드 값이 같아야한다.
+        Teacher teacher1 = new Teacher("이동윤","코리아IT");
+        Teacher teacher2 = new Teacher("이동윤","코리아IT");
+        System.out.println(teacher1.toString());
+        System.out.println(teacher1.hashCode());
+        System.out.println(teacher2.hashCode());
+        System.out.println(teacher1.equals(teacher2));
 
     }
 }
--- src/_26_Objects/Teacher.java
+++ src/_26_Objects/Teacher.java
@@ -1,5 +1,7 @@
 package _26_Objects;
 
+import java.util.Objects;
+
 public class Teacher {
     private String name;
     private String schoolName;
@@ -13,4 +15,24 @@ public class Teacher {
         this.schoolName = schoolName;
     }
 
+    @Override
+    public String toString() {
+        return name + "선생님의 근무지는" + schoolName + "입니다";
+    }
+    //해시코드랑 이퀄스는 한개 오버라이드하면 두개 다 같이 오버라이드 해야함.
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, schoolName);
+    }
+    //name과 schoolName의 값을 기반으로 고유한 숫자(hashcode)를 생성
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Teacher)) return false;
+        Teacher teacher = (Teacher) obj;
+        return this.name.equals(teacher.name) && this.schoolName.equals(teacher.schoolName);
+    }
+    //equals에서 같다고 판별된 객체는 같은 해시코드를 가져야한다. 라고 자바 공식규칙
 }
